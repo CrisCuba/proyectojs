04427ee7c7f071c2dca88996e351bf8cb6a253fa
CrisCuba <cristian.gonzalezcuba@gmail.com>
2021-09-27 23:00:52 -0300

    segunda entrega algoritmo condicional

---
+++ js/entregaCondicional.js
@@ -0,0 +1,7 @@
+let numero = prompt("Ingrese un número");
+
+if ((numero >= "0") && (numero <= "50")){
+    console.log ("Número correcto");}  
+    else {
+        console.log ("Ha ingresado un número incorrecto")
+    }
